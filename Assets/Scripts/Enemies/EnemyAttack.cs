eef06192ffcdd7d65ba416061ccb59fc1134fc8a
Santiago Vaca <santiago.gamedevs@gmail.com>
2025-01-11 21:19:28 -0600

    Enemy animator

--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -3,14 +3,9 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public void StartAttack(float damage, Transform player, float attackCooldown)
+    public void Attack(float damage, Transform player, float attackCooldown)
     {
-        StartCoroutine(AttackCooldown(attackCooldown));
-        player.GetComponent<PlayerHealth>().TakeDamage(damage);
-    }
-
-    public IEnumerator AttackCooldown(float attackCooldown)
-    {
-        yield return new WaitForSeconds(attackCooldown);
+        //player.GetComponent<PlayerHealth>().TakeDamage(damage);
+        Debug.Log("Attacking player for " + damage + " damage");
     }
 }
--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -5,27 +5,59 @@ public class EnemyManager : MonoBehaviour
     public EnemyData enemyData;
 
     private Transform player;
+    private Animator animator;
     private EnemyMovement enemyMovement;
     private EnemyAttack enemyAttack;
+
+    private bool canAttack = true;
+    private float attackCooldown;
 
     private void Start() 
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;   
+        animator = GetComponent<Animator>();
 
         enemyMovement = GetComponent<EnemyMovement>();
         enemyAttack = GetComponent<EnemyAttack>();
+
+        this.attackCooldown = enemyData.attackCooldown;
     }
     private void Update() 
     {
         if(enemyMovement.InAttackRange(enemyData.attackRange, player))
         {
             enemyMovement.StopMovement();
+            animator.SetBool("Moving", false);
 
-            enemyAttack.StartAttack(enemyData.damage, player, enemyData.attackCooldown);
+            if(canAttack)
+            {
+                animator.SetBool("Attacking", true);
+                enemyAttack.Attack(enemyData.damage, player, enemyData.attackCooldown);
+            }
+            else
+                StartCooldown();
         }
         else
         {
             enemyMovement.MoveToAttackRange(enemyData.speed, player);
+            animator.SetBool("Moving", true);
+            animator.SetBool("Attacking", false);
         }
+    }
+
+    public void StartCooldown()
+    {
+        attackCooldown -= Time.deltaTime;
+        if(attackCooldown <= 0)
+        {
+            canAttack = true;
+            attackCooldown = enemyData.attackCooldown;
+        }
+    }
+
+    public void FinishedAttack()
+    {
+        canAttack = false;
+        animator.SetBool("Attacking", false);
     }
 }
